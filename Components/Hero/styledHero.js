b9dd04d35b43f09bcf0448acd659762639b622d2
Omar <omaar_5@hotmail.com>
2021-11-02 00:37:15 +0200

    style: Hero part-2

--- Components/Hero/styledHero.js
+++ Components/Hero/styledHero.js
@@ -1,51 +1,75 @@
 import styled from 'styled-components';
 
 export const HeroBox = styled.div`
-  background: linear-gradient(to bottom, rgba(62, 6, 95, 0.5), rgba(0, 0, 0, 0.6), rgba(62, 6, 95, 0.5)),
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.43), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.43)),
   url('/home.jpeg') center center / cover no-repeat;
   height: 100vh;
-  width: 100%;
-  color: #fff;
   display: flex;
+  flex-direction: column;
+  color: hsla(0, 100%, 100%, 1);
+`;
+
+export const HeroNav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 5rem;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.1));
+
+  @media screen and (max-width: 768px) {
+    padding: 0 2.5rem;
+  }
+
+  @media screen and (max-width: 425px) {
+    padding: 0 1rem;
+  }
+
+  @media screen and (max-width: 320px) {
+    padding: 0 0.2rem;
+  }
+`;
+
+export const NavBox = styled.div`
+ display: flex;
+ align-items: center;
+ cursor: pointer;
+ gap: 1rem;
+
+  @media screen and (max-width: 425px) {
+    gap: 0.5rem;
+  }
+`;
+
+export const HeroLogo = styled.div`
+  width: clamp(3rem, 4.6vw, 5.5rem);
+`;
+
+export const HeroLogoTitle = styled.p`
+  font-size: clamp(1.3rem, 1.6vw, 1.6rem);
 `;
 
 export const HeroFeature = styled.div`
   align-self: center;
-  margin: 0 auto;
-  max-width: 650px;
+  margin: auto;
   text-align: center;
+  padding: 0.3rem;
+
 `;
 
-export const HeroTitle = styled.h3`
-  font-size: clamp(2.092rem, 6vw, 4rem);
-  margin: 0 0 1rem;
-`;
-
-export const HeroSubTitle = styled.h4`
-  font-size: clamp(1.125rem, 3vw, 1.625rem);
-  margin: 1rem 0;
-`;
-
-export const ButtonsBox = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-`;
-
-export const Button = styled.button`
-  border: 0;
-  outline: 0;
-  font-size: 1.5rem;
-  background: rgba(149, 1, 1, 0.7);
-  color: #fff;
+export const HeroTitle = styled.div`
+  font-size: clamp(3rem, 3.5vw, 4rem);
   font-weight: 700;
-  letter-spacing: 0.1rem;
-  padding: 0.5rem 2rem;
-  border-radius: 0.4rem;
-  cursor: pointer;
-  transition: all 0.5s ease-in-out;
+`;
 
-  :hover {
-    background-color: rgba(149, 1, 1, 1);
-  }
+export const HeroSubtitle = styled.p`
+  font-size: clamp(2.5rem, 2.5vw, 3rem);
+  font-weight: 500;
+`;
+
+export const HeroButtons = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
 `;
