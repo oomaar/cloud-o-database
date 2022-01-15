833178e08e783defb280c9f5482e9381418f59ea
Omar <omaar_5@hotmail.com>
2022-01-15 23:36:29 +0200

    mock up

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { Hero } from "../components";
 import { NextHead } from "../global";
 
 const Home = () => {
@@ -5,7 +6,13 @@ const Home = () => {
         <div>
             <NextHead title="Cloud-O" />
 
-            <h1>Home</h1>
+            <Hero />
+
+            <h1>This website will hold all my Data</h1>
+            <h1>authenticate your self with protected route to safel store your data</h1>
+            <h1>route for movies, route for music, route for images, route for programs and a route for emergency Data</h1>
+            <h1>design it well and proffesional</h1>
+            <h1>tools: mongoDB, NextJS, styled-components, images from pexels and logos from my Photoshop</h1>
         </div>
     );
 };
