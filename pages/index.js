c99f7387e421515fc4be64bfb4d2c94a29b50bcd
Omar Hassan <omaar_5@hotmail.com>
2022-03-29 15:02:50 +0200

    Add a comment code [sample login logic]

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,30 @@
 import { Hero } from "../components";
 import { NextHead } from "../global";
 
+// import { useEffect } from "react"
+// import { useRouter } from "next/router"
+// import authService from "auth-service" //imaginary auth service
+
+// export default function ProtectedPage() {
+//  const router = useRouter()
+
+//  useEffect(() => {
+  //  const authUser = authService.getUser()
+
+    // if there is no authenticated user, redirect to login page_
+
+    //if (!authUser) {
+      //router.push("/login")
+    //}
+  //}, [])
+
+  //return (
+    //<>
+      //<h1>Welcome {user.name}. This is a protected page</h1>`
+    //</>
+  //)
+//}
+
 const Home = () => {
     return (
         <div>
